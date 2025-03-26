37ed404255a31dc66ead33315c3551f9420bc047
Min0522-sku <wkdalstj0522@gmail.com>
2025-03-26 12:54:19 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+w = input().strip()
+if w == 'M':
+    print("MatKor")
+elif w == 'W':
+    print("WiCys")
+elif w == 'C':
+    print("CyKor")
+elif w == 'A':
+    print("AlKor")
+elif w == '$':
+    print("$clear")
