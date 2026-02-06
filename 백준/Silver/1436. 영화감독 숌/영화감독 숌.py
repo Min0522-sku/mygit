8be1f3153d2f1f0a0260174d639560f45bfaaf93
Min이 <wkdalstj0522@gmail.com>
2026-02-06 16:45:15 +0900

    [Silver V] Title: 영화감독 숌, Time: 680 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,9 +1,11 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-count = 0
-num = 666
-while count < N:
-    if '666' in str(num):
-        count += 1
-    if count < N:
-        num += 1
+count = 0 # 666 나온 갯수 찾기
+num = 0 # 0에서 증가하는 변수
+while  count < N: # count 가 N에 도달하면 종료
+    num +=1
+    if "666" in  str(num): # num을 str 타입으로 강제 변환 했을때 666이 존재하면
+        count +=1 #count 에 +1
 print(num)
