31091ea60b2cb0a83b4c131dfec727fa283c2290
Min이 <wkdalstj0522@gmail.com>
2026-02-19 18:01:09 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,11 +1,15 @@
+import sys
+input = sys.stdin.readline
+
 N, K = map(int, input().split())
-A = []
-for i in range(N):
-    b = int(input().strip())
-    A.append(b)
-A.sort(reverse=True)
+coinList =[]
+for _ in range(N):
+    coinList.append(int(input().strip()))
+
+coinList.reverse() ## 제일 큰수부터 몫을 구하기 위해서 뒤집음
 count = 0
-for i in A:
-    count += K//i
-    K %= i
+
+for i in coinList:
+    count += K//i  ## 목표값을 coinList의 요소로 나눈 몫(나눌수 있다면) 만큼 카운트에 추가
+    K %= i ## 목표값을 요소로 나눈 나머지로 수정
 print(count)
